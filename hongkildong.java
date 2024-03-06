ad491b404671a7b4f3ab6d83af099f8eb510df41
tpdus751 <152615034+tpdus751@users.noreply.github.com>
2024-03-06 17:36:08 +0900

    수업 시간 코딩

---
+++ hongkildong.java
@@ -0,0 +1,16 @@
+package sec04;
+
+public class hongkildong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "홍길동";
+		int myAge = 20;
+		
+		System.out.println("이름 : " + myName );
+		System.out.println("나이 : " + myAge );
+		System.out.println();
+		System.out.println("이름 : " + myName + "     나이 : myAge " );
+	}
+
+}
